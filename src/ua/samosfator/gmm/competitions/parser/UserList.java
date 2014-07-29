36dbbd1163e877549eff055d1344064ac8cda9d4
samosfator <golubev.vld@yandex.ru>
2014-07-29 17:36:51 +0300

    Migration to LinkedHashSet

--- src/ua/samosfator/gmm/competitions/parser/UserList.java
+++ src/ua/samosfator/gmm/competitions/parser/UserList.java
@@ -5,11 +5,12 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 
 public class UserList {
-    private HashSet<User> users = new HashSet<>();
+    private LinkedHashSet<User> users = new LinkedHashSet<>();
 
-    public HashSet<User> readUserList(String filename) {
+    public LinkedHashSet<User> readUserList(String filename) {
         try (BufferedReader br = new BufferedReader(new FileReader(new File(filename)))) {
             String line;
             while ((line = br.readLine()) != null) {
